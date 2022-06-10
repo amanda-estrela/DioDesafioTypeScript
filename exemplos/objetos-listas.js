b6d50abf06c1e669607bedd29a37bb48fbd12d01
Amanda Estrela <efernandes.amanda@gmail.com>
2022-06-09 23:52:25 -0300

    Desafios 1 e 2 Bootcamp Dio

---
+++ exemplos/objetos-listas.js
@@ -0,0 +1,52 @@
+"use strict";
+/*const pessoa = {
+    nome: "Amanda",
+    idade: 32,
+    profissao: "desenvolvedora"
+}
+
+pessoa.idade = 26
+
+const max: {nome: string, idade:number, profissao:string} = {
+    nome:"Max",
+    idade: 2,
+    profissao:'modelo'
+}
+
+enum Profissao {
+    Professora,
+    Desenvolvedora,
+    Atleta
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: Profissao
+}
+
+interface Estudante extends Pessoa {
+     materias: string[]
+}
+
+const vanessa: Pessoa = {
+    nome: "Vanessa",
+    idade:32,
+    profissao: Profissao.Desenvolvedora
+}
+
+const leticia: Estudante={
+    nome: 'Letícia',
+    idade:21,
+    profissao: Profissao.Desenvolvedora,
+    materias: ['história', 'administração', 'filosofia']
+}
+
+function listar(lista:string[]){
+    for(const item of lista){
+        console.log('- ', item);
+    }
+}
+
+listar(leticia.materias)
+*/ 
